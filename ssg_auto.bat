ef1d8eec4ee2f9a07c6d5daa82454527037b262d
tony-ngok <tonyn1999@outlook.com>
2024-11-01 10:53:42 -0500

    自动化脚本

---
+++ ssg_auto.bat
@@ -0,0 +1,11 @@
+@REM 在Windows中，可运行taskschd.msc，创建一个新计画任务、定时（例如每日执行一次）、调用此脚本，即可实现定时自动抓取。
+
+@echo off
+cd /d "%~dp0"
+pip install -U scrapy
+pip install -U bs4
+git pull
+
+scrapy crawl category
+scrapy crawl product-url
+scrapy crawl product
