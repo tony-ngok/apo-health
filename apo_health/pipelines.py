82f822e858438a69bb38e1e2ac8abc73959d84bc
tony-ngok <tonyn1999@outlook.com>
2024-11-02 13:39:35 -0500

    no ES

--- apo_health/pipelines.py
+++ apo_health/pipelines.py
@@ -10,60 +10,60 @@ import json
 import sys
 import time
 
-from em_product.resources.pipelines import (
-    ESCategoryPipeline,
-    ESProductUrlPipeline,
-    ESProductPipeline,
-    ESProductRecrawlPipeline,
-    ESTranslationPipeline,
-    ESSeedProductPipeline,
-)
+# from em_product.resources.pipelines import (
+#     ESCategoryPipeline,
+#     ESProductUrlPipeline,
+#     ESProductPipeline,
+#     ESProductRecrawlPipeline,
+#     ESTranslationPipeline,
+#     ESSeedProductPipeline,
+# )
 from itemadapter import ItemAdapter
 import pymongo
 from pymongo.errors import ConnectionFailure, NetworkTimeout
 from scrapy import Spider
 from scrapy.crawler import Crawler
 
-from apo_health.pymongo_utils import ausverkaufte, bulk_write, get_uos
+from pymongo_utils import ausverkaufte, bulk_write, get_uos
 
 
-class CategoryPipeline(ESCategoryPipeline):
-    ELASTICSEARCH_BUFFER_LENGTH = 1
-    CATEGORY_INDEX = "apo_health_categories"
-    ELASTICSEARCH_INDEX = CATEGORY_INDEX
+# class CategoryPipeline(ESCategoryPipeline):
+#     ELASTICSEARCH_BUFFER_LENGTH = 1
+#     CATEGORY_INDEX = "apo_health_categories"
+#     ELASTICSEARCH_INDEX = CATEGORY_INDEX
 
 
-class ProductUrlPipeline(ESProductUrlPipeline):
-    FORCE = True
-    CATEGORY_INDEX = "apo_health_categories"
-    PRODUCT_URL_INDEX = "apo_health_product_urls"
-    ELASTICSEARCH_INDEX = PRODUCT_URL_INDEX
+# class ProductUrlPipeline(ESProductUrlPipeline):
+#     FORCE = True
+#     CATEGORY_INDEX = "apo_health_categories"
+#     PRODUCT_URL_INDEX = "apo_health_product_urls"
+#     ELASTICSEARCH_INDEX = PRODUCT_URL_INDEX
 
 
-class ProductPipeline(ESProductPipeline):
-    FORCE = True
-    PRODUCT_URL_INDEX = "apo_health_product_urls"
-    PRODUCT_INDEX = "apo_health_products"
-    ELASTICSEARCH_INDEX = PRODUCT_INDEX
+# class ProductPipeline(ESProductPipeline):
+#     FORCE = True
+#     PRODUCT_URL_INDEX = "apo_health_product_urls"
+#     PRODUCT_INDEX = "apo_health_products"
+#     ELASTICSEARCH_INDEX = PRODUCT_INDEX
 
 
-class SeedProductPipeline(ESSeedProductPipeline):
-    PRODUCT_URL_INDEX = "apo_health_product_urls"
-    PRODUCT_INDEX = "apo_health_products"
-    ELASTICSEARCH_INDEX = PRODUCT_INDEX
+# class SeedProductPipeline(ESSeedProductPipeline):
+#     PRODUCT_URL_INDEX = "apo_health_product_urls"
+#     PRODUCT_INDEX = "apo_health_products"
+#     ELASTICSEARCH_INDEX = PRODUCT_INDEX
 
 
-class RecrawlProductPipeline(ESProductRecrawlPipeline):
-    PRODUCT_INDEX = "apo_health_products"
-    ELASTICSEARCH_INDEX = PRODUCT_INDEX
-    PRODUCT_ID_NAME = "product_id"
+# class RecrawlProductPipeline(ESProductRecrawlPipeline):
+#     PRODUCT_INDEX = "apo_health_products"
+#     ELASTICSEARCH_INDEX = PRODUCT_INDEX
+#     PRODUCT_ID_NAME = "product_id"
 
 
-class TranslationPipeline(ESTranslationPipeline):
-    TRANSLATION_INDEX = "apo_health_translation"
-    # ELASTICSEARCH_BUFFER_LENGTH = 5
-    ELASTICSEARCH_BUFFER_LENGTH = 1
-    ELASTICSEARCH_INDEX = TRANSLATION_INDEX
+# class TranslationPipeline(ESTranslationPipeline):
+#     TRANSLATION_INDEX = "apo_health_translation"
+#     # ELASTICSEARCH_BUFFER_LENGTH = 5
+#     ELASTICSEARCH_BUFFER_LENGTH = 1
+#     ELASTICSEARCH_INDEX = TRANSLATION_INDEX
 
 
 class MongoPipeLine:
